8d6e41fac802fcf3094b2d325bfc9bf783160d49
Yuri Ppo <yuri.prata12@gmail.com>
2024-11-22 14:46:39 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+
+def main():
+    print("Welcome to the Band Name Generator App.")
+
+    city_name = input("What's the name of the city you grew up in?\n")
+
+    pet_name = input("What's your pet's name?\n")
+
+
+    print("Your band name could be: "+ city_name +" "+ pet_name)
+
+if __name__=="__main__":
+    main()
